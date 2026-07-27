0404678618f5f260f1c33b15fbed570d9bedb9a7
callradar <sollbe82@gmail.com>
2026-07-27 14:29:57 +0900

    bump-vc21-open-redesign-line

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.callradar.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 20
-        versionName = "2.0"
+        versionCode = 21
+        versionName = "2.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
